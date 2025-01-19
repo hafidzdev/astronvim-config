41c68d240af40d56770b133a47e9b6a28a4789c3
hafidz <hafidzdev@gmail.com>
2025-01-19 15:02:53 +0700

    adjust color scheme

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
   -- import/override with your plugins folder
 }
