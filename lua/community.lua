6400afe0d4bc8928dd2165c9ffad847075ac66ef
hafidz <hafidzdev@gmail.com>
2025-01-20 13:35:18 +0700

    update plugin

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.nightfox-nvim" },
   -- import/override with your plugins folder
